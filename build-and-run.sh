ce55f5b598be49d292ce54e3e2f00d79891a53b1
Arun Chandrasekaran <aruncxy@gmail.com>
2017-12-08 16:27:43 -0800

    include dmd build

--- build-and-run.sh
+++ build-and-run.sh
@@ -1,9 +1,13 @@
 g++ --version
 g++ -std=c++11 -O3 main.cpp -o cppmain -pthread
 ldc2 --version
-ldc2 main.d -of=dmain -O5
-num=2000000
-iter=200
+ldc2 main.d -of=dmain-ldc -O5
+dmd --version
+dmd main.d -of=dmain-dmd -O
+num=1000000
+iter=5
 ./cppmain $(nproc) $num $iter > /dev/null
-echo
-./dmain $(nproc) $num $iter > /dev/null
+echo --- LDC ---
+./dmain-ldc $(nproc) $num $iter > /dev/null
+echo --- DMD ---
+./dmain-dmd $(nproc) $num $iter > /dev/null
